0e2eb675f25c922fbe0191765a7220750900abdb
Jorge Herraiz Soler <estudiantejorgeherraiz@gmail.com>
2025-07-29 20:12:12 +0200

    Agregar archivos de proyecto.

---
+++ RetailApp.Backend/Models/Store.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetailApp.Backend.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(255)]
+        public string? Address { get; set; }
+
+        [StringLength(50)]
+        public string? City { get; set; }
+
+        [StringLength(20)]
+        public string? PhoneNumber { get; set; }
+
+        [StringLength(100)]
+        public string? Email { get; set; }
+
+        public DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
+
+        // Propiedad
+        public 
+    }
+}
