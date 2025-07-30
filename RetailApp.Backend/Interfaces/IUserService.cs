b04f6c6638fffe34ad079cd4522fde6680f53759
Jorge Herraiz Soler <estudiantejorgeherraiz@gmail.com>
2025-07-30 16:08:58 +0200

    Creada la carpeta Interfaces y  la interfaz IUserService

--- RetailApp.Backend/Interfaces/IUserService.cs
+++ RetailApp.Backend/Interfaces/IUserService.cs
@@ -1,6 +1,12 @@
 ﻿namespace RetailApp.Backend.Interfaces
 {
-    public class IUserService
+    public interface IUserService // Interfaz para el servicio de Usuarios
     {
+        Task<IEnumerable<User>> GetAllUsersAsync(); // Método para obtener todos los usuarios
+        Task<User?> GetUserByIdAsync(int id); // Método para obtener un usuario por ID
+        Task<User> CreateUserAsync(User user); // Método para crear un nuevo usuario
+        Task<bool> UpdateUserAsync(User user); // Método para actualizar un usuario existente
+        Task<bool> DeleteUserAsync(int id); // Método para eliminar un usuario por ID
+
     }
 }
